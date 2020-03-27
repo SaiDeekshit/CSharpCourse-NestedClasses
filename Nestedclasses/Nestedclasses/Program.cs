c1afd65a004c1b6ceaf763958fe897c817924534
Sai Deekshith <msaideekshith@gmail.com>
2020-03-27 07:36:48 +0530

    use of nested classes

---
+++ Nestedclasses/Nestedclasses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Nestedclasses
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Animals animal = new Animals();
+            Animals.Dogs dog = new Animals.Dogs();
+            dog.dogBreed = "Greman Shepard";
+
+            Animals.Cats cat = new Animals.Cats();
+            cat.catName = "Rex";
+
+            Console.WriteLine("DogBreed " + dog.dogBreed);
+            Console.WriteLine("Cat Name " + cat.catName);
+        }
+    }
+    //Parent Class
+    class Animals
+    {
+        public string animalName;
+        public string animalCountry;
+
+        //Child Classes
+        public class Dogs
+        {
+            public string dogBreed;
+            public string dogNickname;
+        }
+
+        public class Cats
+        {
+            public string catName;
+        }
+
+    }
+}
